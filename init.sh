3798e49e7f2243b0b4783c01316e49367d247a6f
FYYFU <a17638939549@gmail.com>
2025-07-04 00:39:34 +0000

    update init

--- init.sh
+++ init.sh
@@ -4,3 +4,6 @@ git config --global user.email a17638939549@gmail.com
 git config --global user.name FYYFU
 
 sudo apt install python3.10-dev
+
+
+wget https://github.com/Dao-AILab/flash-attention/releases/download/v2.5.8/flash_attn-2.5.8+cu122torch2.3cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
